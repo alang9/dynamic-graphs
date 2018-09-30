22929ddb9c58c4d1f316c3845215df9fbd476f92
Jasper Van der Jeugt <m@jaspervdj.be>
2018-09-30 14:53:54 -0400

    Slow graphs, generate programs

---
+++ tests/Data/Graph/Dynamic/Program.hs
@@ -0,0 +1,105 @@
+module Data.Graph.Dynamic.Program
+    ( Program
+    , Instruction (..)
+
+    , IntTreeProgram (..)
+    , IntGraphProgram (..)
+    ) where
+
+import qualified Data.Graph.Dynamic.Slow as Slow
+import           Data.Hashable           (Hashable)
+import qualified Data.HashSet            as HS
+import qualified Test.QuickCheck         as QC
+
+type Program v = [Instruction v]
+
+data Instruction v
+    = InsertVertex v
+    | InsertEdge v v Bool
+    | DeleteVertex v
+    | DeleteEdge v v Bool
+    | Connected v v Bool
+    deriving (Show)
+
+genProgram
+    :: (Eq v, Hashable v)
+    => Bool          -- ^ Acyclic only
+    -> Int           -- ^ Size of program
+    -> Slow.Graph v  -- ^ State of the graph
+    -> [v]           -- ^ Pool of Vs to use
+    -> QC.Gen (Program v)
+genProgram _ size _ _ | size <= 0 = return []
+genProgram acyclic size0 graph0 vs0 = do
+    let hasVertices = not $ null $ Slow.vertices graph0
+    mbInstruction <- QC.frequency $
+        [(10, genInsertVertex)] ++
+        [(20, genInsertEdge) | hasVertices] ++
+        [(1,  genDeleteVertex) | hasVertices] ++
+        [(10, genDeleteEdge) | hasVertices] ++
+        [(10, genConnected) | hasVertices]
+
+    case mbInstruction of
+        Nothing -> genProgram acyclic size0 graph0 vs0
+        Just (instr, graph1, vs1) -> (instr :) <$>
+            genProgram acyclic (size0 - 1) graph1 vs1
+  where
+    genInsertVertex =
+        let (v, vs1) = case vs0 of
+                []       -> error "Ran out of Vs..."
+                (x : xs) -> (x, xs)
+
+            graph1 = Slow.insertVertex v graph0 in
+
+        return $ Just (InsertVertex v, graph1, vs1)
+
+    genInsertEdge = do
+        x <- QC.elements $ Slow.vertices graph0
+        y <- QC.elements $ Slow.vertices graph0
+        if Slow.connected x y graph0 && acyclic then
+            return Nothing
+        else
+            let (res, graph1) = Slow.insertEdge x y graph0 in
+            return $ Just (InsertEdge x y res, graph1, vs0)
+
+    genDeleteVertex = do
+        v <- QC.elements $ Slow.vertices graph0
+        let graph1 = Slow.deleteVertex v graph0
+        return $ Just (DeleteVertex v, graph1, v : vs0)
+
+    genDeleteEdge = do
+        x <- QC.elements $ Slow.vertices graph0
+        let nbs = HS.toList $ Slow.neighbours x graph0
+        if null nbs then
+            return Nothing
+        else do
+            y <- QC.elements nbs
+            let (res, graph1) = Slow.deleteEdge x y graph0
+            return $ Just (DeleteEdge x y res, graph1, vs0)
+
+    genConnected = do
+        x <- QC.elements $ Slow.vertices graph0
+        y <- QC.elements $ Slow.vertices graph0
+        let res = Slow.connected x y graph0
+        return $ Just (Connected x y res, graph0, vs0)
+
+newtype IntTreeProgram = IntTreeProgram {unIntTreeProgram :: Program Int}
+    deriving (Show)
+
+instance QC.Arbitrary IntTreeProgram where
+    arbitrary = QC.sized $ \size -> fmap IntTreeProgram $
+        genProgram True size Slow.empty [1 ..]
+
+    shrink = fmap IntTreeProgram . shrinkList . unIntTreeProgram
+
+newtype IntGraphProgram = IntGraphProgram {unIntGraphProgram :: Program Int}
+    deriving (Show)
+
+instance QC.Arbitrary IntGraphProgram where
+    arbitrary = QC.sized $ \size -> fmap IntGraphProgram $
+        genProgram False size Slow.empty [1 ..]
+
+    shrink = fmap IntGraphProgram . shrinkList . unIntGraphProgram
+
+shrinkList :: [a] -> [[a]]
+shrinkList []       = []
+shrinkList (x : xs) = xs : map (x :) (shrinkList xs)
---
+++ tests/Data/Graph/Dynamic/Slow.hs
@@ -0,0 +1,79 @@
+-- | A very slow but simple and hence probably correct implementation against we
+-- can check our proper implementations.
+module Data.Graph.Dynamic.Slow
+    ( Graph
+    , empty
+    , insertVertex
+    , insertEdge
+    , deleteVertex
+    , deleteEdge
+    , hasEdge
+    , connected
+    , neighbours
+    , vertices
+    ) where
+
+import           Data.Hashable       (Hashable)
+import qualified Data.HashMap.Strict as HMS
+import qualified Data.HashSet        as HS
+import qualified Data.List           as List
+import           Data.Maybe          (fromMaybe)
+
+newtype Graph v = Graph
+    { unGraph :: HMS.HashMap v (HS.HashSet v)
+    } deriving (Show)
+
+empty :: Graph v
+empty = Graph HMS.empty
+
+insertVertex :: (Eq v, Hashable v) => v -> Graph v -> Graph v
+insertVertex v = Graph . HMS.insertWith HS.union v HS.empty . unGraph
+
+insertEdge :: (Eq v, Hashable v) => v -> v -> Graph v -> (Bool, Graph v)
+insertEdge x y g0 = case HMS.lookup x (unGraph g0) of
+    Nothing  -> (False, g0)
+    Just set
+        | y `HS.member` set -> (False, g0)
+        | otherwise         ->
+            let g1 = Graph $
+                    HMS.insertWith HS.union x (HS.singleton y) $
+                    HMS.insertWith HS.union y (HS.singleton x) $
+                    unGraph g0 in
+            (True, g1)
+
+deleteVertex :: (Eq v, Hashable v) => v -> Graph v -> Graph v
+deleteVertex x g0 =
+    List.foldl' (\g n -> snd (deleteEdge x n g)) g0 (neighbours x g0)
+
+deleteEdge :: (Eq v, Hashable v) => v -> v -> Graph v -> (Bool, Graph v)
+deleteEdge x y g0 = case HMS.lookup x (unGraph g0) of
+    Nothing  -> (False, g0)
+    Just set
+        | y `HS.member` set ->
+            let g1 = Graph $
+                    HMS.adjust (HS.delete y) x $
+                    HMS.adjust (HS.delete x) y $
+                    unGraph g0 in
+            (True, g1)
+        | otherwise         -> (False, g0)
+
+neighbours :: (Eq v, Hashable v) => v -> Graph v -> HS.HashSet v
+neighbours x g = fromMaybe HS.empty $ HMS.lookup x (unGraph g)
+
+hasEdge :: (Eq v, Hashable v) => v -> v -> Graph v -> Bool
+hasEdge x y g = y `HS.member` neighbours x g
+
+connected :: (Eq v, Hashable v) => v -> v -> Graph v -> Bool
+connected x y g = go HS.empty (HS.singleton x)
+  where
+    go visited queue = case HS.toList queue of
+        []                          -> False
+        (q : _)
+            | q `HS.member` visited -> go visited (HS.delete q queue)
+            | q == y                -> True
+            | otherwise             ->
+                let new = neighbours q g `HS.difference` visited in
+                go (HS.insert q visited) (new `HS.union` HS.delete q queue)
+
+vertices :: (Eq v, Hashable v) => Graph v -> [v]
+vertices = map fst . HMS.toList . unGraph
