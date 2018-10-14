d4ed84bba04b5cccffc7ec9667917c2af6a95e45
Jasper Van der Jeugt <m@jaspervdj.be>
2018-10-14 13:23:14 +0100

    Further test cleanup, merge Tests2 and Tests

--- tests/Data/Graph/Dynamic/EulerTour/Tests.hs
+++ tests/Data/Graph/Dynamic/EulerTour/Tests.hs
@@ -8,14 +8,51 @@
 
 module Data.Graph.Dynamic.EulerTour.Tests where
 
+import           Control.Monad                        (foldM)
 import           Control.Monad.ST
+import           Data.Graph.Dynamic.Action
+import qualified Data.Graph.Dynamic.EulerTour         as ET
+import qualified Data.Graph.Dynamic.Internal.Random   as Random
+import           Data.Graph.Dynamic.Internal.Tree     (Tree)
+import           Data.Graph.Dynamic.Program
+import qualified Data.Graph.Dynamic.Slow              as Slow
+import           Data.Hashable                        (Hashable)
+import           Data.List                            (mapAccumL)
+import           Data.Maybe                           (catMaybes)
 import           Test.Framework
 import           Test.Framework.Providers.QuickCheck2
 import           Test.Framework.TH
+import qualified Test.QuickCheck                      as QC
 
-import qualified Data.Graph.Dynamic.EulerTour         as ET
-import qualified Data.Graph.Dynamic.Internal.Random   as Random
-import           Data.Graph.Dynamic.Program
+runForestAction
+    :: (Eq v, Hashable v, Monoid a, Tree tree)
+    => ET.Forest tree a s v -> [Bool] -> Action t v -> ST s [Bool]
+runForestAction etf xs (Cut x y) = ET.deleteEdge etf x y >> return xs
+runForestAction etf xs (Link x y) = ET.insertEdge etf x y >> return xs
+runForestAction etf xs (Toggle x y) = ET.hasEdge etf x y >>= \case
+  True -> ET.deleteEdge etf x y >> return xs
+  False -> ET.insertEdge etf x y >> return xs
+runForestAction etf xs (Query x y) = ET.connected etf x y >>= \case
+  Nothing -> return xs
+  Just q -> return (q:xs)
+
+checkActions :: QC.Positive Int -> [Action t Int] -> QC.Property
+checkActions (QC.Positive n) actions = slowResult QC.=== result
+  where
+    actions' = map (fmap (`mod` n)) actions
+    initialGraph = Slow.fromVertices [0..n-1]
+    slowResult = catMaybes $ snd $ mapAccumL runSlowForestAction initialGraph actions'
+    result :: [Bool]
+    result = runST $ do
+      initialForest <- ET.discreteForest' (\_ _ -> ()) [0..n-1]
+      results <- foldM (runForestAction initialForest) [] actions'
+      return $ reverse results
+
+prop_forest_linkcut :: QC.Positive Int -> [Action 'LinkCut Int] -> QC.Property
+prop_forest_linkcut = checkActions
+
+prop_forest_toggle :: QC.Positive Int -> [Action 'Toggl Int] -> QC.Property
+prop_forest_toggle = checkActions
 
 prop_program :: IntTreeProgram -> ()
 prop_program (IntTreeProgram p) = runST go
--- tests/Data/Graph/Dynamic/Levels/Tests.hs
+++ tests/Data/Graph/Dynamic/Levels/Tests.hs
@@ -8,13 +8,58 @@
 
 module Data.Graph.Dynamic.Levels.Tests where
 
+import           Control.Monad                        (foldM, void)
 import           Control.Monad.ST
+import           Data.Graph.Dynamic.Action
 import qualified Data.Graph.Dynamic.Internal.Random   as Random
+import           Data.Graph.Dynamic.Internal.Tree     (Tree)
 import qualified Data.Graph.Dynamic.Levels            as Levels
 import           Data.Graph.Dynamic.Program
+import qualified Data.Graph.Dynamic.Slow              as Slow
+import           Data.Hashable                        (Hashable)
+import           Data.List                            (mapAccumL)
+import           Data.Maybe                           (catMaybes)
 import           Test.Framework
 import           Test.Framework.Providers.QuickCheck2
 import           Test.Framework.TH
+import qualified Test.QuickCheck                      as QC
+
+runGraphAction
+    :: (Eq v, Hashable v, Tree tree)
+    => Levels.Graph tree s v -> [Bool] -> Action t v -> ST s [Bool]
+runGraphAction levels xs (Cut x y) = do
+    Levels.deleteEdge levels x y
+    return xs
+runGraphAction levels xs (Link x y) = do
+  _ <- Levels.insertEdge levels x y
+  return xs
+runGraphAction levels xs (Toggle x y) = do
+  Levels.hasEdge levels x y >>= \case
+    True  -> Levels.deleteEdge levels x y
+    False -> void $ Levels.insertEdge levels x y
+  return xs
+runGraphAction levels xs (Query x y) = Levels.connected levels x y >>= \case
+  Nothing -> return xs
+  Just q -> do
+    return (q:xs)
+
+checkActions :: QC.Positive Int -> [Action t Int] -> QC.Property
+checkActions (QC.Positive n) actions = slowResult QC.=== result
+  where
+    actions' = map (fmap (`mod` n)) actions
+    initialSlowGraph = Slow.fromVertices [0..n-1]
+    slowResult = catMaybes $ snd $ mapAccumL runSlowGraphAction initialSlowGraph actions'
+    result :: [Bool]
+    result = runST $ do
+      initialGraph <- Levels.fromVertices' [0..n-1]
+      results <- foldM (runGraphAction initialGraph) [] actions'
+      return $ reverse results
+
+prop_graph_linkcut :: QC.Positive Int -> [Action 'LinkCut Int] -> QC.Property
+prop_graph_linkcut = checkActions
+
+prop_graph_toggle :: QC.Positive Int -> [Action 'Toggl Int] -> QC.Property
+prop_graph_toggle = checkActions
 
 prop_program :: IntGraphProgram -> ()
 prop_program (IntGraphProgram p) = runST go
